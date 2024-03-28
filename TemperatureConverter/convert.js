d6cac5a20b68dd5af34b8d1b18d5381873b4d786
cwoltman <158247085+cwoltman@users.noreply.github.com>
2024-03-27 23:56:47 -0500

    update image

--- TemperatureConverter/convert.js
+++ TemperatureConverter/convert.js
@@ -9,7 +9,8 @@ function domLoaded() {
     const cInput = document.getElementById("cInput");
     const fInput = document.getElementById("fInput");
     const errorMessage = document.getElementById("errorMessage");
- 
+    const weatherImage = document.getElementById("weatherImage");
+
     errorMessage.textContent = "";
  
     const celsius = parseFloat(cInput.value);
@@ -22,14 +23,31 @@ function domLoaded() {
  
     if (!isNaN(celsius)) {
        fInput.value = convertCtoF(celsius);
+       fInput.value = convertedFahrenheit;
+       updateImage(convertedFahrenheit);
     }
  
     if (!isNaN(fahrenheit)) {
        cInput.value = convertFtoC(fahrenheit);
+       cInput.value = convertedCelsius;
+       updateImage(fahrenheit);
+    }
+ }
+
+ function updateImage(temperature) {
+    const weatherImage = document.getElementById("weatherImage");
+    if (temperature < 32) {
+       weatherImage.src = "cold.png";
+       weatherImage.alt = "Cold";
+    } else if (temperature >= 32 && temperature <= 50) {
+       weatherImage.src = "cool.png";
+       weatherImage.alt = "Cool";
+    } else {
+       weatherImage.src = "warm.png";
+       weatherImage.alt = "Warm";
     }
  }
  
-
 function convertCtoF(degreesCelsius) {
    return (degreesCelsius * 9/5) +32;
 }

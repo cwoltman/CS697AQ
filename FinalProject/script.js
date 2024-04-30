1f8f4bdcdaeb2bdba78522206020a19111b92a88
cwoltman <158247085+cwoltman@users.noreply.github.com>
2024-04-29 21:56:02 -0500

    Website Front-End Framework

---
+++ FinalProject/script.js
@@ -0,0 +1,30 @@
+// script.js
+
+// Function to handle form submission
+document.getElementById('productSearchForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    // Fetch form data
+    const productName = document.getElementById('productName').value;
+
+    // Call a function to perform search based on the form data
+    performSearch(productName);
+});
+
+// Function to perform search and display results
+function performSearch(productName) {
+    
+    const searchResultsContainer = document.getElementById('searchResults');
+    searchResultsContainer.innerHTML = `<p>Search results for: ${productName}</p>`;
+}
+
+// Function to clear form fields
+document.getElementById('clearButton').addEventListener('click', function() {
+    // Clear form fields
+    document.getElementById('productName').value = '';
+});
+
+// Function to navigate back to the product search page
+function performAnotherSearch() {
+    window.location.href = 'index.html';
+}

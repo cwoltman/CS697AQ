fe63156740066c094ae13432129d93e59a080c60
cwoltman <158247085+cwoltman@users.noreply.github.com>
2024-04-30 19:01:59 -0500

    Updated JavScript

--- FinalProject/script.js
+++ FinalProject/script.js
@@ -1,29 +1,26 @@
 // script.js
 
-// Function to handle form submission
-document.getElementById('productSearchForm').addEventListener('submit', function(event) {
-    event.preventDefault();
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById('productSearchForm').addEventListener('submit', function(event) {
+        event.preventDefault();
 
-    const productName = document.getElementById('productName').value;
-    const warehouseCity = document.getElementById('warehouseCity').value;
-    const minPrice = document.getElementById('minPrice').value;
-    const maxPrice = document.getElementById('maxPrice').value;
-    const minQuantity = document.getElementById('minQuantity').value;
-    const maxQuantity = document.getElementById('maxQuantity').value;
+        const productName = document.getElementById('productName').value;
+        const warehouseCity = document.getElementById('warehouseCity').value;
+        const minPrice = document.getElementById('minPrice').value;
+        const maxPrice = document.getElementById('maxPrice').value;
+        const minQuantity = document.getElementById('minQuantity').value;
+        const maxQuantity = document.getElementById('maxQuantity').value;
 
-    window.location.href = `search_results.html?productName=${productName}&warehouseCity=${warehouseCity}&minPrice=${minPrice}&maxPrice=${maxPrice}&minQuantity=${minQuantity}&maxQuantity=${maxQuantity}`;
+        window.location.href = `search_results.html?productName=${productName}&warehouseCity=${warehouseCity}&minPrice=${minPrice}&maxPrice=${maxPrice}&minQuantity=${minQuantity}&maxQuantity=${maxQuantity}`;
+    });
+
+    document.getElementById('clearButton').addEventListener('click', function() {
+        // Clear form fields
+        document.getElementById('productName').value = '';
+        document.getElementById('warehouseCity').value = '';
+        document.getElementById('minPrice').value = '';
+        document.getElementById('maxPrice').value = '';
+        document.getElementById('minQuantity').value = '';
+        document.getElementById('maxQuantity').value = '';
+    });
 });
-
-document.getElementById('clearButton').addEventListener('click', function() {
-    // Clear form fields
-    document.getElementById('productName').value = '';
-    document.getElementById('warehouseCity').value = '';
-    document.getElementById('minPrice').value = '';
-    document.getElementById('maxPrice').value = '';
-    document.getElementById('minQuantity').value = '';
-    document.getElementById('maxQuantity').value = '';
-});
-
-function performAnotherSearch() {
-    window.location.href = 'index.html';
-}
